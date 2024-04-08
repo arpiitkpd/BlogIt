ecda304e71f85296f8c67876d27d39451c4ad0bb
arpiitkpd <arpiitkumar02@gmail.com>
2024-04-08 16:00:23 +0530

    Created the appwrite services with all required functions of the database, also created the components and the pages with all working functionality

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import {useSelector} from 'react-redux'
+import{useNavigate} from 'react-router-dom'
+import Loader from '../components/loader/Loader'
+
+function Protected({children, authentication = true}) {
+  
+  const authStatus = useSelector((state)=> state.auth.status)
+
+  const navigate = useNavigate()
+  const [loader, setLoader] =useState(true)
+
+  useEffect(()=>{
+    if(authentication  && authStatus != authentication){
+      navigate('/login')
+    }else if(!authentication && authStatus!== authentication){
+      navigate('/')
+    }
+    setLoader(false)
+  }, [authStatus,authentication, navigate])
+  
+  return (
+    loader ?  <Loader/> : <>{children}</>
+  )
+}
+
+export default Protected
